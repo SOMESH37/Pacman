8f62602fbe35c73ef20531206a9fadbba3514a05
SOMESH37 <someshmira@gmail.com>
2020-09-04 21:40:33 +0530

    Added gameover and win functions, food and pills are now significant to pacman, ghosts can be scared now

--- js.js
+++ js.js
@@ -12,7 +12,7 @@ const layout = [
     1,1,1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,1,1,
     1,1,1,1,1,1,0,1,1,4,1,4,1,2,2,1,4,1,4,1,1,0,1,1,1,1,1,1,
     1,1,1,1,1,1,0,1,1,4,1,4,1,2,2,1,4,1,4,1,1,0,1,1,1,1,1,1,
-    0,0,0,0,0,0,0,0,0,0,1,4,1,1,1,1,4,1,0,0,0,0,0,0,0,0,0,0,
+    4,0,0,0,0,0,0,0,0,0,1,4,1,1,1,1,4,1,0,0,0,0,0,0,0,0,0,4,
     1,1,1,1,1,1,0,1,1,0,1,4,4,0,0,4,4,1,0,1,1,0,1,1,1,1,1,1,
     1,1,1,1,1,1,0,1,1,0,1,4,1,1,1,1,4,1,0,1,1,0,1,1,1,1,1,1,
     1,1,1,1,1,1,0,1,1,0,1,4,4,4,4,4,4,1,0,1,1,0,1,1,1,1,1,1,
@@ -36,6 +36,8 @@ const layout = [
 ]
 //create board
 const squares = []
+let pacCurrent;
+let score = 0;
 for (let i = 0; i < layout.length; i++) {
       const square = document.createElement('div');
       document.querySelector('.grid').appendChild(square);
@@ -47,10 +49,13 @@ function createBoard() {
         squares[i].classList.add('food');
       } else if (layout[i] === 1) {
         squares[i].classList.add('wall');
+      } else if (layout[i] === 2) {
+        squares[i].classList.add('ghost');
       } else if (layout[i] === 3) {
         squares[i].classList.add('pill');
       } else if (layout[i] === 5 ) {
         squares[i].classList.add('pac');
+        pacCurrent=i;
       }
     }	clearInterval(CB);
 }
@@ -86,10 +91,117 @@ function moveGhost(ghost) {
       if  (!squares[ghost.current + nextmove].classList.contains('ghost') 
            && !squares[ghost.current + nextmove].classList.contains('wall') ) {
           //transfer the ghost
-          squares[ghost.current].classList.remove(ghost.name, 'ghost');
+          squares[ghost.current].classList.remove(ghost.name, 'ghost', 'scared-ghost');
           ghost.current += nextmove;
           squares[ghost.current].classList.add(ghost.name, 'ghost');
       
       } else nextmove = moves[Math.floor(Math.random() * moves.length)];
+
+      if (ghost.scared) {
+        squares[ghost.current].classList.add('scared-ghost');
+      }
+
+      //if the ghost is currently scared and pacman hits him
+      if(ghost.scared && squares[ghost.current].classList.contains('pac')) {
+        squares[ghost.current].classList.remove(ghost.name, 'ghost', 'scared-ghost');
+        ghost.current = ghost.start;
+        score +=50;
+        document.getElementById('score').innerHTML = score;
+        squares[ghost.current].classList.add('ghost');
+      }
+    checkForGameOver();
+    checkForWin();
     }, ghost.speed);
 }
+//movement of pacman
+function movePacman(e) {
+    squares[pacCurrent].classList.remove('pac');
+    switch(e.keyCode) {
+      case 37:
+        if(
+          pacCurrent % 28 !== 0 &&
+          !squares[pacCurrent -1].classList.contains('wall') &&
+          !squares[pacCurrent -1].classList.contains('ghost')
+          ){
+        pacCurrent -= 1;
+          }
+        if (squares[pacCurrent-1] === squares[363]) {
+          pacCurrent = 391;
+        }
+        break;
+      case 38:
+        if(
+          pacCurrent - 28 >= 0 &&
+          !squares[pacCurrent -28].classList.contains('wall') &&
+          !squares[pacCurrent -28].classList.contains('ghost')
+          ) {
+             pacCurrent -= 28;
+            }
+        break;
+      case 39:
+        if(
+          pacCurrent % 28 < 28 - 1 &&
+          !squares[pacCurrent +1].classList.contains('wall') &&
+          !squares[pacCurrent +1].classList.contains('ghost')
+        ){
+        pacCurrent += 1;         
+           }
+        if (squares[pacCurrent +1] === squares[392]) {
+          pacCurrent = 364;
+        }
+        break;
+      case 40:
+        if (
+          pacCurrent + 28 < (28 * 28) &&
+          !squares[pacCurrent +28].classList.contains('wall') &&
+          !squares[pacCurrent +28].classList.contains('ghost')
+        ){
+        pacCurrent += 28;
+         }
+        break;
+    }
+    squares[pacCurrent].classList.add('pac');
+    foodEaten();
+    pillEaten();
+    checkForGameOver();
+    checkForWin();
+  }
+document.addEventListener('keyup', movePacman);
+
+//eat a food
+function foodEaten() {
+    if (squares[pacCurrent].classList.contains('food')) {
+      score++;
+      document.getElementById('score').innerHTML = score;
+      squares[pacCurrent].classList.remove('food');
+    }
+}
+
+//eat a pill
+function pillEaten() {
+    if (squares[pacCurrent].classList.contains('pill')) {
+      score +=10;
+      document.getElementById('score').innerHTML = score;
+      ghosts.forEach(ghost => ghost.scared = true);
+      setTimeout( function() { ghosts.forEach(ghost => ghost.scared = false); }, 10000);
+      squares[pacCurrent].classList.remove('pill');
+    }
+}
+
+function checkForGameOver() {
+    if (squares[pacCurrent].classList.contains('ghost') &&
+      !squares[pacCurrent].classList.contains('scared-ghost')) {
+      document.removeEventListener('keyup', movePacman);
+      ghosts.forEach(ghost => clearInterval(ghost.ID));
+      document.getElementById('something').classList.add('gameover');
+    }
+}
+
+function checkForWin() {
+    if (score >= 500) {
+	  ghosts.forEach(ghost => clearInterval(ghost.ID));
+      document.removeEventListener('keyup', movePacman);
+      document.getElementById('something').classList.add('win'); 
+    }
+}
+
